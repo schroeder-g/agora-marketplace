e2d7c33db88cb7395114ac5dddb3727180d7ba36
kevin lam <kevin@kevins-MacBook-Pro-3.local>
2020-09-19 02:40:22 -0700

    initial file setup for backend app

---
+++ src/test/java/com/lambdaschool/african_market_place/AfricanMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.african_market_place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfricanMarketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
